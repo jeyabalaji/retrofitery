bc0a650f3b3a6cd7fbe326ff031a7c77ea271c61
jeyabalaji <jeyabalaji.s@dal.ca>
2018-12-01 23:39:05 -0400

    Added comment to clarify @Query

--- app/src/main/java/components/android/jeyabalaji/retrofit/retrofit/PlaceAutoCompleteAPI.java
+++ app/src/main/java/components/android/jeyabalaji/retrofit/retrofit/PlaceAutoCompleteAPI.java
@@ -7,6 +7,11 @@ import retrofit2.http.Query;
 
 public interface PlaceAutoCompleteAPI
 {
+    /*
+    * @Query() appends the parameter to the HTTP request.
+    * In this case, the request made by retrofit looks like
+    * BASEURL/api/place/autocomplete/json?types=address&key=YOUR-KEY&input=addressFromUser
+    * */
     @GET("api/place/autocomplete/json?types=address&key=YOUR-KEY")
     Call<PredictionResponse> loadPredictions(@Query("input") String address);
 }
